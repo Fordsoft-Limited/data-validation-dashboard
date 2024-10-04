2108cf25241386b580b3461150430067e92df91e
Ahmad Dolapo Asiyanbola <dolapo@fordsoft.tech>
2024-10-04 10:36:10 +0100

    update

--- src/app/pages/data-validation/model/bulk-validation.ts
+++ src/app/pages/data-validation/model/bulk-validation.ts
@@ -1,8 +1,84 @@
-export interface DataValidation{
+// export interface DataValidation{
+//     customerFullName?:string;
+//     accountNo?:string;
+//     meterNo?:string;
+//     address?:string;
+//     city?:string;
+//     lga?:string;
+//     state?:string;
+//     latitude?:string;
+//     longitude?:string;
+//     customerId?:string;
+//     cin?:string;
+//     applicationDate?:string|Date;
+//     currentTariffCode?:string;
+//     accountType?:string;
+//     correctTariffCode?:string;
+//     tariffClass?:string;
+
+//     email?:string;
+//     mobile?:string;
+//     id?:string;
+//     feeder?:string;
+//     district?:string;
+//     date?: string | Date;
+//     status?:string;
+//     comments?: string;
+// }
+export interface DataValidation {
     id?:string;
-    feeder?:string;
-    district?:string;
+    customerFullName: string;
+    accountNo: string;
+    meterNo: string;
+    address: string;
+    city: string;
+    lga: string;
+    state: string;
+    nearestLandmark: string;
     date?: string | Date;
-    status?:string;
+    latitude: string;
+    longitude: string;
+    customerId: string;
+    cin: string;
+    applicationDate:string | Date; 
+    mobile: string;
+    email: string;
+    statusCode: string; 
+    accountType: string; 
+    currentTariffCode: string; 
+    correctTariffCode: string;
+    tariffClass: string; 
+    feeder: string;
+    feederId: string;
+    serviceCenter: string;
+    distributionName: string;
+    dssId: string;
+    ltPoleId: string;
+    serviceWire: string;
+    upriser: string;
+    region: string;
+    businessHub: string;
+    accountCategory: string; 
+    connectionType: string; 
+    custNatureOfBusiness: string; 
+    customerNIN: string;
+    customerSupplyType: string;
+    customerEstimatedLoad: string; 
+    custHasMeter: string; 
+    customerMeterCategory: string; // e.g., 'Postpaid - Electronics'
+    customerMeterManufacturer: string;
+    customerMeterSaled: string; 
+    customerMeterAccessible: string; 
+    customerMeterLocation: string; 
+    customerBillName: string;
+    customerHasAccountNo: string; 
+    customerGroup: string; 
+    isLandlord: string; 
+    landlordName: string;
+    landlordPhone: string;
+    tenantName: string;
+    tenantPhone: string;
+    meterCTRatio: string; // e.g., '1:1'
     comments?: string;
-}
+    status?:string;
+  }
