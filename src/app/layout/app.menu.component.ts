29de11b946fd8a4f6f9a8859981f14668e2896b1
Ahmad Dolapo Asiyanbola <dolapo@fordsoft.tech>
2024-10-18 01:12:26 +0100

    timeline

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -18,7 +18,7 @@ export class AppMenuComponent implements OnInit {
                  { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/app'] },
                  {label: 'User Management', icon: 'pi pi-fw pi-users', routerLink: ['/app/user-management'] },
                  {label: 'Checker', icon: 'pi pi-fw pi-check-square', routerLink: ['/app/data-validation'] },
-                 {label: 'Bulk Upload', icon: 'pi pi-fw pi-users', routerLink: ['/app/customer-validation'] },
+                 {label: 'Bulk Upload', icon: 'pi pi-fw pi-sitemap', routerLink: ['/app/customer-validation'] },
                 //  {label: 'Customer Information', icon: 'pi pi-fw pi-users', routerLink: ['/app/customer-information'] },
                 {label: 'New Customer', icon: 'pi pi-fw pi-users', routerLink: ['/app/new-customer'] },
                  {label: 'Reports', icon: 'pi pi-fw pi-file', routerLink: ['/app/reports'] },
