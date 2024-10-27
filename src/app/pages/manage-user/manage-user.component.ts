f8062c58dd3dc14a6bf0587bd5c0a5575c61f4d5
Johnson Akanmu <johnsonakanmu2017@gmail.com>
2024-10-27 15:59:14 +0100

    latest

--- src/app/pages/manage-user/manage-user.component.ts
+++ src/app/pages/manage-user/manage-user.component.ts
@@ -3,6 +3,7 @@ import { MessageService, ConfirmationService } from 'primeng/api';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { addUser } from '../../model/user';
 import { UserService } from '../../api/user.service';
+import { AuthService } from '../../auth/service/auth.service';
 
 @Component({
   selector: 'app-manage-user',
@@ -29,7 +30,7 @@ export class ManageUserComponent implements OnInit {
   successMessage: string = '';      // Success message text
   errorMessage: string = '';   
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private authService: AuthService) {
     this.role = [
       { name: 'Admin', code: 'ADMIN' },
       { name: 'User', code: 'USER' },
@@ -126,8 +127,18 @@ onSubmit() {
       role: this.userForm.value.role,
       password: this.userForm.value.password
     };
-  
-    this.userService.createUser(payload,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMwMDQzNjA0LCJpYXQiOjE3Mjk5NTcyMDQsImp0aSI6IjhiNTkwY2RlN2JjMDQ5Y2RhZDQ2MzE2ZjQwZGZhOTVhIiwidXNlcl9pZCI6MSwibmFtZSI6Ik9kb2ZpbiBPeWVqaWRlIiwicm9sZSI6IkFETUlOIiwic2x1ZyI6Im95ZWppZGUiLCJ1c2VybmFtZSI6Im95ZWppZGUiLCJpc19hY3RpdmUiOnRydWV9.PcvKj6bsjJD_t14SjRGwlQ4sqGOzzJFgStlzLz76ONY").subscribe({
+    const token = this.authService.getToken();
+    console.log(token);
+  if (!token) {
+    this.isLoading = false;
+    this.userAddedError = true;
+    this.errorMessage = 'No authentication token found. Please log in again.';
+    setTimeout(() => {
+      this.userAddedError = false;
+    }, 3000);
+    return; // Exit the function early
+  }
+    this.userService.createUser(payload,token).subscribe({
       next: (response) => {
         this.userAddedSuccess = true;
         this.successMessage = 'Success! Your account has been successfully created!';
